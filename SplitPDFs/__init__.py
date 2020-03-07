9298efcae832a04192a26394c843a773eb1e72ac
Steve <demarco.st@gmail.com>
2020-03-07 12:12:04 -0500

    Initial commit

---
+++ SplitPDFs/__init__.py
@@ -0,0 +1,59 @@
+import logging
+import base64
+import io
+import PyPDF2
+import json
+import datetime
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Get the request body from previous step in Logic App
+    req_body = req.get_json()
+    file_name = req_body.get('fileName')
+    file_content = req_body['fileContent']['$content']
+
+    # Convert file content to bytes
+    file_data = base64.decodebytes(file_content.encode())
+
+    # Generate date time object of the run
+    dt = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+    # Extract original PDF file name
+    pdf_prefix_file_name = ''.join(file_name.split('.pdf')[:1]) + '_'
+    
+    # Open multi-page PDF file
+    with io.BytesIO(decoded) as open_pdf_file:
+        read_pdf = PyPDF2.PdfFileReader(open_pdf_file)
+
+        # Extract each page and write out to individual files
+        pdf_list = []
+        for i in range(read_pdf.numPages):
+            output = PyPDF2.PdfFileWriter()
+            output.addPage(read_pdf.getPage(i))
+            
+            # Temporarily write PDF to disk
+            temp_pdf_fn = pdf_prefix_file_name + str(i + 1) + '_' + dt + str(".pdf")
+            with open(temp_pdf_fn, "wb") as outputStream:
+                output.write(outputStream)
+
+            # Read back in the PDF to get the bytes-like version
+            with open(temp_pdf_fn, 'rb') as temp_pdf_file:
+                file_data = base64.b64encode(temp_pdf_file.read()).decode()
+
+                # Record the filename and the bytes-like data of single-page PDF
+                ind_pdf = {
+                    'fileName': temp_pdf_fn,
+                    'fileContent': file_data
+                }
+
+            pdf_list.append(ind_pdf)
+    
+    # Finalize response object
+    respObj = {
+        'individualPDFs': pdf_list
+    }
+
+    return func.HttpResponse(body=json.dumps(respObj))
